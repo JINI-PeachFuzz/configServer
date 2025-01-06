d2035318ff3e8c03b2366a0a83ff510a3f9b4e5e
JINJU PARK <aricclunj@naver.com>
2025-01-06 23:57:08 +0900

    01.06 초기 설정

--- src/main/java/org/jiniit/ConfigserverApplication.java
+++ src/main/java/org/jiniit/ConfigserverApplication.java
@@ -2,7 +2,9 @@ package org.jiniit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer // 설정서버를 활성화 시켜줌
 @SpringBootApplication
 public class ConfigserverApplication {
 
